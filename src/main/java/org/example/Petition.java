405759cd98de3fa9d8e8874c56b49a834659330f
KateSweeney97 <katsweeney97@gmail.com>
2023-11-26 18:01:54 +0000

    Updated Petition Java Class with Getters and Setters

--- src/main/java/org/example/Petition.java
+++ src/main/java/org/example/Petition.java
@@ -1,30 +1,42 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.sql.Timestamp;
 import java.util.List;
 
 public class Petition {
     private String title;
     private String description;
     private List<Signature> signatures;
+    private String id;
 
     public Petition(String title, String description) {
         this.title = title;
         this.description = description;
         this.signatures = new ArrayList<>();
+        this.id = String.valueOf(new Timestamp(System.currentTimeMillis()).getTime());
     }
 
     // Getters and setters
-    public void addSignature(String name, String email) {
-        Signature s = new Signature();
-        s.name = name;
-        s.email = email;
-        signatures.add(s);
+    public String getId() {
+        return this.id;
     }
 
-    private class Signature {
-        String name;
-        String email;
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public List<Signature> getSignatures() {
+        return this.signatures;
+    }
+
+    public void addSignature(String name, String email) {
+        Signature s = new Signature(name, email);
+        signatures.add(s);
     }
 }
 
