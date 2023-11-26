8103b268dc65e58d290f9e39a107685cb94c5cb3
KateSweeney97 <katsweeney97@gmail.com>
2023-11-26 18:23:42 +0000

    changed main to extend springbootserveltinitialiser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
         System.out.println("Hello world! Yay");
         SpringApplication.run(Main.class, args);
