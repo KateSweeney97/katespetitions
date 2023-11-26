f1dcfec7997a7664d820dbfc7d0add88bd132f45
KateSweeney97 <katsweeney97@gmail.com>
2023-11-26 18:01:29 +0000

    Added Signature Java Class

---
+++ src/main/java/org/example/Signature.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Signature {
+    private String name;
+    private String email;
+    public Signature(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
